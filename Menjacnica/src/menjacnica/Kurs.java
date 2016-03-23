31764267ce54556bbc883bb4ae9705e9982e6877
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-03-23 17:10:09 +0100

    Napravljene klase Valuta i Kurs i izgenerisane get i set metode za sve atribute ovih klasa.

---
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -0,0 +1,37 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+
+	private double prodajniKurs;
+	private double srednjiKurs;
+	private double kupovniKurs;
+	private GregorianCalendar datum;
+	
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	
+}
---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,21 @@
+package menjacnica;
+
+public class Valuta {
+	
+	private String imeValute;
+	private String skracenica;
+	
+	public String getImeValute() {
+		return imeValute;
+	}
+	public void setImeValute(String imeValute) {
+		this.imeValute = imeValute;
+	}
+	public String getSkracenica() {
+		return skracenica;
+	}
+	public void setSkracenica(String skracenica) {
+		this.skracenica = skracenica;
+	}
+	
+}
