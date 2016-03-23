f02eae795ec28d75cc4b4e89baf5b08253975569
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-03-23 19:43:08 +0100

    Uvedene equals, hashcode i toString metode u klase Kurs i Valuta.

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -26,5 +26,44 @@ public class Valuta {
 	public void setImeValute(String imeValute) {
 		this.imeValute = imeValute;
 	}
+	@Override
+	public String toString() {
+		return "Valuta [imeValute=" + imeValute + ", skracenica=" + skracenica + ", kursevi=" + kursevi + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((imeValute == null) ? 0 : imeValute.hashCode());
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (imeValute == null) {
+			if (other.imeValute != null)
+				return false;
+		} else if (!imeValute.equals(other.imeValute))
+			return false;
+		if (kursevi == null) {
+			if (other.kursevi != null)
+				return false;
+		} else if (!kursevi.equals(other.kursevi))
+			return false;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		return true;
+	}
 	
 }
